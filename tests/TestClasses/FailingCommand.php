a9b8b9c5524de1b33819e130db82a4af9f71b9a6
freek <freek@spatie.be>
2020-09-29 16:46:34 +0200

    wip

--- tests/TestClasses/FailingCommand.php
+++ tests/TestClasses/FailingCommand.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\ScheduleMonitor\Tests\TestClasses;
 
+use Exception;
 use Illuminate\Console\Command;
 
 class FailingCommand extends Command
@@ -12,7 +13,6 @@ class FailingCommand extends Command
 
     public function handle()
     {
-        sleep(60);
-        throw new \Exception('failing');
+        throw new Exception('failing');
     }
 }
