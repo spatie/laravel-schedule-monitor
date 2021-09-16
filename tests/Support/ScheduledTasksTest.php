41c1936c65de1113be0ad64f005ac782969520b4
renkensmenno <renkensmenno@gmail.com>
2021-09-16 10:23:04 +0200

    add test

--- tests/Support/ScheduledTasksTest.php
+++ tests/Support/ScheduledTasksTest.php
@@ -3,6 +3,7 @@
 namespace Spatie\ScheduleMonitor\Tests\Support;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Config;
 use Spatie\ScheduleMonitor\Support\ScheduledTasks\ScheduledTasks;
 use Spatie\ScheduleMonitor\Support\ScheduledTasks\Tasks\Task;
 use Spatie\ScheduleMonitor\Tests\TestCase;
@@ -37,5 +38,24 @@ class ScheduledTasksTest extends TestCase
         $this->assertEquals([
             'dummy-closure',
         ], $duplicateTasks);
+    }
+    
+    /** @test */
+    public function it_can_get_only_the_tasks_that_run_in_the_current_environment_from_the_schedule()
+    {
+        TestKernel::registerScheduledTasks(function (Schedule $schedule) {
+            $schedule->command('dummy')->environments('testing'); // current
+            $schedule->command('other-dummy')->environments('production'); 
+        });
+
+        $scheduledTasks = ScheduledTasks::createForSchedule();
+
+        $uniqueTasks = $scheduledTasks->uniqueTasks()
+            ->map(fn (Task $task) => "{$task->name()}-{$task->type()}")
+            ->toArray();
+
+        $this->assertEquals([
+            'dummy-command',
+        ], $uniqueTasks);
     }
 }
