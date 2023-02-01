0224bc86760eaa08e3e36c66c6f03ff43c764049
freekmurze <freekmurze@users.noreply.github.com>
2023-02-01 19:18:52 +0000

    Fix styling

--- tests/Jobs/SilenceOhDearJobTest.php
+++ tests/Jobs/SilenceOhDearJobTest.php
@@ -2,7 +2,7 @@
 
 use Spatie\ScheduleMonitor\Jobs\PingOhDearJob;
 
-it('will silence the PingOhDearJob by default', function() {
+it('will silence the PingOhDearJob by default', function () {
     config()->set('horizon.silenced', [PingOhDearJob::class]);
 
     expect(config('horizon.silenced'))->toContain(PingOhDearJob::class);
