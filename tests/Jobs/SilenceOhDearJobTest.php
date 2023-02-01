c918de2834181f09353b6e43e1b954be3ba09ca2
Freek Van der Herten <freek@spatie.be>
2023-02-01 20:25:27 +0100

    remove failing test

--- tests/Jobs/SilenceOhDearJobTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Spatie\ScheduleMonitor\Jobs\PingOhDearJob;
-
-it('will silence the PingOhDearJob by default', function () {
-    config()->set('horizon.silenced', [PingOhDearJob::class]);
-
-    expect(config('horizon.silenced'))->toContain(PingOhDearJob::class);
-});
